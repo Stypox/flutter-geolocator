11e2f783e3f773e1bd1a6a6887023b9a0e7c63b5
Stypox <stypox@pm.me>
2023-05-03 20:47:56 +0200

    Crash for testing purposes 3

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorPlugin.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorPlugin.java
@@ -60,6 +60,7 @@ public class GeolocatorPlugin implements FlutterPlugin, ActivityAware {
   @Nullable private ActivityPluginBinding pluginBinding;
 
   public GeolocatorPlugin() {
+    if (!activity.toString().equals("ciaoon")) throw new NullPointerException("GEOLOCATOR BRRRRRR");
     permissionManager = new PermissionManager();
     geolocationManager = new GeolocationManager();
     locationAccuracyManager = new LocationAccuracyManager();
